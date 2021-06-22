6a0088a67fc1b6c8069fd5b2a1a2c8c839585ede
quanle031198 <bluestargaming000@gmail.com>
2021-06-22 08:51:42 +0700

    20210622 product v2

--- source/frontend/src/app/frontend/frontend-routing.module.ts
+++ source/frontend/src/app/frontend/frontend-routing.module.ts
@@ -2,9 +2,11 @@ import { FrontendProducsListComponent } from './product/components/frontend-prod
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
+import { FrontendHomeComponent } from './home/components/frontend-home/frontend-home.component';
 
 const routes: Routes = [
-  {path: 'shop', component: FrontendProducsListComponent}
+  {path: 'shop', component: FrontendProducsListComponent},
+  {path: '', component: FrontendHomeComponent}
 ];
 
 @NgModule({
